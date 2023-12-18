6912c55a5217ba62fd082a77c2e684778019d856
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2023-12-18 08:31:35 +0000

    making fixes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch, SafeAreaView } from 'react-native';
+import { ThemeContext } from './src/context/themeContext';
+import { myColors } from './src/styles/Colors';
+import Button from './src/components/Button';
+import MyKeyboard from './src/components/MyKeyboard';
 
 export default function App() {
+  const [theme, setTheme] = useState('dark');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ThemeContext.Provider value={theme}>
+    <SafeAreaView style={theme === 'dark' ? styles.container : [styles.container, {backgroundColor: '#17171C'}]}>
       <StatusBar style="auto" />
-    </View>
+      <Switch 
+        value = {theme === 'dark'}
+        onValueChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        />
+        <MyKeyboard />
+    </SafeAreaView>
+     </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
---
+++ src/styles/Colors.tsx
@@ -0,0 +1,12 @@
+export const myColors = {
+    light: '#fff',
+    dark: '#17171C',
+    blue: '#4B5EFC',
+    btnGray: '#4E505F', 
+    btnDark: '#2E2F38',
+    gray: '#747477',
+    black: '#000000',
+    white: '#FFFFFF',
+    result: '#46D582',
+    orange: '#FFA500'
+}
